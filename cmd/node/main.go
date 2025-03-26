430da16e2c4a27492682ac02a7b1ff23c4467e2f
taptap-kor <tyy8282@gmail.com>
2025-03-26 23:19:59 +0900

    feat: db init

--- cmd/node/main.go
+++ cmd/node/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
@@ -8,29 +9,37 @@ import (
 
 	"github.com/abcfe/abcfe-node/app"
 	"github.com/abcfe/abcfe-node/common/logger"
+	"github.com/abcfe/abcfe-node/storage"
 )
 
 func main() {
-	nodeApp, err := app.New()
+	app, err := app.New()
 	if err != nil {
 		log.Fatalf("Failed to initialize application: %v", err)
 		os.Exit(1)
 	}
 
-	if err := logger.InitLogger(&nodeApp.Conf); err != nil {
+	if err := logger.InitLogger(&app.Conf); err != nil {
 		log.Fatalf("Failed to initialize logger: %v", err)
 		os.Exit(1)
 	}
+
+	db, err := storage.InitDB(&app.Conf)
+	if err != nil {
+		logger.Error("Failed to load db: ", err)
+	}
+
+	fmt.Println("temp db log: ", db)
 
 	sigCh := make(chan os.Signal, 1)
 	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM) // OS 시그널을 채널로 전달
 	go func() {
 		sig := <-sigCh
 		logger.Info("Arrived terminate signal: ", sig)
-		nodeApp.Terminate()
+		app.Terminate()
 	}()
 
 	logger.Info("Node start.")
-	nodeApp.Wait()
+	app.Wait()
 	logger.Info("Node terminated.")
 }
