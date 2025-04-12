e5dee0c3ebaa248a0525564f9393ae2b5730b3d5
taptap-kor <tyy8282@gmail.com>
2025-04-13 03:03:43 +0900

    feat: set tx, test code for mempool and block set

--- core/transaction.go
+++ core/transaction.go
@@ -1,10 +1,15 @@
 package core
 
-import prt "github.com/abcfe/abcfe-node/protocol"
+import (
+	"time"
+
+	"github.com/abcfe/abcfe-node/common/utils"
+	prt "github.com/abcfe/abcfe-node/protocol"
+)
 
 // 트랜잭션 구조체
 type Transaction struct {
-	Version   uint32   `json:"version"`   // 트랜잭션 버전
+	Version   string   `json:"version"`   // 트랜잭션 버전
 	ID        prt.Hash `json:"id"`        // 트랜잭션 ID (해시)
 	Timestamp int64    `json:"timestamp"` // 트랜잭션 생성 시간
 
@@ -17,10 +22,10 @@ type Transaction struct {
 
 type TxInput struct {
 	TxID        prt.Hash      `json:"txId"`        // 참조 트랜잭션 ID
-	OutputIndex uint32        `json:"outputIndex"` // 참조 출력 인덱스
+	OutputIndex uint64        `json:"outputIndex"` // 참조 출력 인덱스
 	Signature   prt.Signature `json:"signature"`   // 서명
 	PublicKey   []byte        `json:"publicKey"`   // 공개키
-	Sequence    uint32        `json:"sequence"`    // 시퀀스 번호 (RBF 지원)
+	Sequence    uint64        `json:"sequence"`    // 시퀀스 번호 (RBF 지원)
 }
 
 type TxOutput struct {
@@ -29,10 +34,45 @@ type TxOutput struct {
 	TxType  uint8       `json:"txType"`  // 스크립트 타입 (일반/스테이킹/기타)
 }
 
-func SetTx() {}
+func (p *BlockChain) SetTx(txIn []*TxInput, txOut []*TxOutput, memo string, data []byte) {
+	tx := Transaction{
+		Version:   p.cfg.Version.Transaction,
+		Timestamp: time.Now().Unix(),
+		Inputs:    txIn,
+		Outputs:   txOut,
+		Memo:      memo,
+		Data:      data,
+	}
 
-func GetTx(height uint64, id prt.Hash) {}
+	txHash := utils.Hash(tx)
+	tx.ID = txHash
 
-func GetTxStatus(height uint64, id prt.Hash) {}
+}
 
-func GetTxs(height uint64) {}
+func (p *BlockChain) SetTxInput(txID prt.Hash, outIndex uint64, sig prt.Signature, pubKey []byte, seq uint64) *TxInput {
+	txIn := &TxInput{
+		TxID:        txID,
+		OutputIndex: outIndex,
+		Signature:   sig,
+		PublicKey:   pubKey,
+		Sequence:    seq,
+	}
+
+	return txIn
+}
+
+func (p *BlockChain) SetTxOutput(toAddr prt.Address, amount uint64, txType uint8) *TxOutput {
+	txOut := &TxOutput{
+		Address: toAddr,
+		Amount:  amount,
+		TxType:  txType,
+	}
+
+	return txOut
+}
+
+func (p *BlockChain) GetTx(height uint64, id prt.Hash) {}
+
+func (p *BlockChain) GetTxStatus(height uint64, id prt.Hash) {}
+
+func (p *BlockChain) GetTxs(height uint64) {}
--- go.mod
+++ go.mod
@@ -3,13 +3,22 @@ module github.com/abcfe/abcfe-node
 go 1.23.3
 
 require (
+	github.com/lestrrat-go/file-rotatelogs v2.4.0+incompatible
+	github.com/naoina/toml v0.1.1
+	github.com/stretchr/testify v1.8.4
+	github.com/syndtr/goleveldb v1.0.0
+	go.uber.org/zap v1.27.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang/snappy v0.0.0-20180518054509-2e65f85255db // indirect
-	github.com/lestrrat-go/file-rotatelogs v2.4.0+incompatible // indirect
+	github.com/jonboulle/clockwork v0.5.0 // indirect
+	github.com/kylelemons/godebug v1.1.0 // indirect
 	github.com/lestrrat-go/strftime v1.1.0 // indirect
 	github.com/naoina/go-stringutil v0.1.0 // indirect
-	github.com/naoina/toml v0.1.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/syndtr/goleveldb v1.0.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
-	go.uber.org/zap v1.27.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
