8d106d731e921fdd34b2699571fb0eb20c642c82
taptap-kor <tyy8282@gmail.com>
2025-07-06 02:47:49 +0900

    feat: get mempool tx list, add block for test

--- api/rest/routes.go
+++ api/rest/routes.go
@@ -24,7 +24,7 @@ func setupRouter(blockchain *core.BlockChain) http.Handler {
 	api.HandleFunc("/status", GetStatus(blockchain)).Methods("GET")
 
 	// 블록 관련 API
-	// api.HandleFunc("/block", ComposeAndAddBlock(blockchain)).Methods("POST") // 테스트 전용 블록 구성 및 블록 추가 (검증은 없음)
+	api.HandleFunc("/block", ComposeAndAddBlock(blockchain)).Methods("POST") // 테스트 전용 블록 구성 및 블록 추가 (검증은 없음)
 	api.HandleFunc("/block/latest", GetLatestBlock(blockchain)).Methods("GET")
 	api.HandleFunc("/block/{height}", GetBlockByHeight(blockchain)).Methods("GET")
 	api.HandleFunc("/block/hash/{hash}", GetBlockByHash(blockchain)).Methods("GET")
@@ -32,6 +32,9 @@ func setupRouter(blockchain *core.BlockChain) http.Handler {
 	// 트랜잭션 관련 API
 	api.HandleFunc("/tx", SubmitTransferTx(blockchain)).Methods("POST") // tx to mempool
 	api.HandleFunc("/tx/{txid}", GetTx(blockchain)).Methods("GET")
+
+	// 멤풀 관련 API
+	api.HandleFunc("/mempool/list", GetMempoolList(blockchain)).Methods("GET")
 
 	// UTXO 관련 API
 	api.HandleFunc("/address/{address}/utxo", GetAddressUtxo(blockchain)).Methods("GET")
